318a89141c2bfd995901e290bc44db644b5dffaa
William Diangellis <diangellis2014@gmail.com>
2023-09-21 17:38:36 -0300

    Requisição PUT e DELETE ok

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -16,13 +16,13 @@ import med.voll.api.endereco.Endereco;
 @EqualsAndHashCode(of = "id")
 public class Medico {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String telefone;
     private String crm;
-
 
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
@@ -39,12 +39,27 @@ public class Medico {
         this.endereco = new Endereco(dados.endereco());
     }
 
+    public void atualizarInformacoes(DadosAtualizadosMedico dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public Long getId(){
+        return this.id;
+    }
     public String getNome() {
         return this.nome;
     }
 
     public String getEmail() {
-        return this.nome;
+        return this.email;
     }
 
     public String getCrm() {
@@ -54,5 +69,6 @@ public class Medico {
     public Especialidade getEspecialidade() {
         return this.especialidade;
     }
+
 }
 
